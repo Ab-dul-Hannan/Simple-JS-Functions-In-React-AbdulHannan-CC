02125a3c45f00862d269a27262b4f16415e06e43
Abdul Hannan <abdunknownsamurai@gmail.com>
2026-02-06 18:30:01 +0500

    Counter Created using Use States

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -1,20 +1,36 @@
-import React, {usestate} from 'react' 
+import React, { useState } from 'react'
 import styles from './counter.module.css'
-import {ArrowRight, ArrowLeft} from 'lucide-react'
+import { ArrowRight, ArrowLeft, Ban } from 'lucide-react'
 
 const counter = () => {
-  return (
-    <div className={styles.counterbox}>
-        <div className={styles.screen}>
-            0
+
+    let [value, setValue] = useState(0)
+
+    return (
+        <div className={styles.counterbox}>
+            <div className={styles.screen}>
+                {value}
+            </div>
+            <div className={styles.playbtns}>
+                <button className={`${styles.btns} ${styles.b1}`}
+                onClick={() => {
+                    value--;
+                    setValue(value);
+                }}><ArrowLeft /></button>
+
+                <button className={`${styles.btns} ${styles.b2}`} 
+                onClick={() => {
+                    setValue(value = 0);
+                }}><Ban /></button>
+
+                <button className={`${styles.btns} ${styles.b3}`} 
+                onClick={() => {
+                    value++;
+                    setValue(value);
+                }}><ArrowRight /></button>
+            </div>
         </div>
-        <div className={styles.playbtns}>
-            <button className={`${styles.btns} ${styles.b1}`}><ArrowLeft /></button>
-            <button className={styles.btns}>2</button>
-            <button className={styles.btns}><ArrowRight /></button>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default counter
